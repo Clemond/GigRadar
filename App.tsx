7c99d489a4b7eaf7533f6400737a891ac70a0d2f
Nicholas <quackshows11@gmail.com>
2025-04-29 13:29:17 +0200

    feat: implement expo linear gradient to be able to have gradient colors

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
   Image
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   const [username, setUsername] = useState("");
@@ -31,64 +32,74 @@ export default function App() {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#1c1822" }}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
-          style={styles.container}
-        >
-          <Image
-            source={require("./assets/gigradar-logo.png")}
-            style={styles.logo}
-            resizeMode="contain"
-          />
-          <View style={styles.card}>
-            <Text style={styles.header}>Welcome Back</Text>
-
-            <TextInput
-              style={styles.input}
-              placeholder="Username"
-              placeholderTextColor="#f8a8d3"
-              value={username}
-              onChangeText={setUsername}
-              contextMenuHidden={true}
+    <LinearGradient
+      colors={["#2A2232", "#e0b0a0", "#150E17"]} // Lighter at the top, darker at the bottom
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      style={styles.gradient}
+    >
+      <SafeAreaView style={{ flex: 1 }}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+            style={styles.container}
+          >
+            <Image
+              source={require("./assets/gigradar-logo.png")}
+              style={styles.logo}
+              resizeMode="contain"
             />
+            <View style={styles.card}>
+              <Text style={styles.header}>Welcome Back</Text>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Password"
-              placeholderTextColor="#f8a8d3"
-              secureTextEntry
-              value={password}
-              onChangeText={setPassword}
-              contextMenuHidden={true}
-            />
+              <TextInput
+                style={styles.input}
+                placeholder="Username"
+                placeholderTextColor="#f8a8d3"
+                value={username}
+                onChangeText={setUsername}
+                contextMenuHidden={true}
+              />
 
-            <TouchableOpacity>
-              <Text style={styles.forgotPassword}>Forgot Password?</Text>
-            </TouchableOpacity>
+              <TextInput
+                style={styles.input}
+                placeholder="Password"
+                placeholderTextColor="#f8a8d3"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+                contextMenuHidden={true}
+              />
 
-            <Text style={styles.orText}>OR</Text>
+              <TouchableOpacity>
+                <Text style={styles.forgotPassword}>Forgot Password?</Text>
+              </TouchableOpacity>
 
-            <TouchableOpacity>
-              <Text style={styles.createAccount}>Create an account?</Text>
-            </TouchableOpacity>
+              <Text style={styles.orText}>OR</Text>
 
-            <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-              <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-          </View>
-        </KeyboardAvoidingView>
-      </TouchableWithoutFeedback>
-    </SafeAreaView>
+              <TouchableOpacity>
+                <Text style={styles.createAccount}>Create an account?</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={handleLogin}
+              >
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1c1822",
+    //backgroundColor: "#1c1822",
     alignItems: "center",
     padding: 20
   },
@@ -153,5 +164,8 @@ const styles = StyleSheet.create({
     width: 120,
     height: 120,
     marginBottom: 20
+  },
+  gradient: {
+    flex: 1
   }
 });

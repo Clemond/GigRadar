62e185d3c295a52888c2ab517795a6fe2cfa3246
Nicholas <quackshows11@gmail.com>
2025-04-30 15:57:41 +0200

    fix: create a firebase config reference file and move firebase config to git ignore

---
+++ firebaseConfig.example.ts
@@ -0,0 +1,10 @@
+// This is a firebase config reference file
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_DOMAIN",
+  projectId: "CHANGE_THIS_TO_YOUR_PROJECTS_ID",
+  storageBucket: "CHANGE_THIS_TO_YOUR_PROJECTS_ID",
+  messagingSenderId: "CHANGE_THIS_TO_YOUR_PROJECTS_ID",
+  appId: "CHANGE_THIS_TO_YOUR_PROJECTS_ID"
+};

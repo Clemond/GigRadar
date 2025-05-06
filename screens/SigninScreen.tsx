b8a7e1843931468e966cd1dc6f7e69a4457732d5
Nicholas <quackshows11@gmail.com>
2025-05-06 22:18:33 +0200

    feat: show dynamic snackbar message instead of hard coded message in SigninScreen

--- screens/SigninScreen.tsx
+++ screens/SigninScreen.tsx
@@ -44,7 +44,7 @@ export default function SigninScreen() {
           label: "Close"
         }}
       >
-        Wrong Credentials
+        {snackbarMsg}
       </Snackbar>
     </SafeAreaView>
   );

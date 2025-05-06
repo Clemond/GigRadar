caf6d5f9a4e8398a2df5269eb3e44a6c621890f1
Nicholas <quackshows11@gmail.com>
2025-05-06 15:30:45 +0200

    feat: create zustand store and utils function for fetching user location

---
+++ utils/loadUserLocation.ts
@@ -0,0 +1,24 @@
+import * as Location from "expo-location";
+import { useLocationStore } from "../stores/useLocationStore";
+import { Alert } from "react-native";
+
+export async function loadUserLocation() {
+  const { setLocation, setCity } = useLocationStore.getState();
+
+  const { status } = await Location.requestForegroundPermissionsAsync();
+  if (status !== "granted") {
+    Alert.alert(
+      "Location PErmission Denied",
+      "We couldn't access your location, Concerts near you may not be shown",
+      [{ text: "OK" }]
+    );
+    return;
+  }
+
+  const location = await Location.getCurrentPositionAsync({});
+  setLocation(location);
+
+  const reverseGeocoded = await Location.reverseGeocodeAsync(location.coords);
+  const city = reverseGeocoded?.[0]?.city ?? "stockholm";
+  setCity(city);
+}

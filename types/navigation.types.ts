7b98cccb55d99ad5d0a0eac3978657455daa3b8b
Nicholas <144162774+Clemond@users.noreply.github.com>
2025-05-07 19:41:30 +0200

    refactor: change name of CreateAccountScreen to RegisterForm (#21)

--- types/navigation.types.ts
+++ types/navigation.types.ts
@@ -1,6 +1,6 @@
 export type RootStackParamList = {
   SigninScreen: undefined;
   HomeScreen: undefined;
-  CreateAccountScreen: undefined;
+  RegisterScreen: undefined;
   ExploreScreen: undefined;
 };

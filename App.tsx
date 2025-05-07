a120c393648962f01896b32d184ceab4bc5856a8
Nicholas <quackshows11@gmail.com>
2025-05-07 17:18:16 +0200

    feat: install tanstack query

--- App.tsx
+++ App.tsx
@@ -4,17 +4,22 @@ import RootStack from "./navigation/navigation";
 import { PaperProvider } from "react-native-paper";
 import { useEffect } from "react";
 import { loadUserLocation } from "./utils/loadUserLocation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
   useEffect(() => {
     loadUserLocation();
   }, []);
 
+  const queryClient = new QueryClient();
+
   return (
-    <NavigationContainer>
-      <PaperProvider>
-        <RootStack />
-      </PaperProvider>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <PaperProvider>
+          <RootStack />
+        </PaperProvider>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }

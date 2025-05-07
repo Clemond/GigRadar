250bf0765b43dd13c798894abf21b4aa63951f99
Nicholas <quackshows11@gmail.com>
2025-05-07 12:40:49 +0200

    add BottomNavBar to ExploreScreen

--- screens/ExploreScreen.tsx
+++ screens/ExploreScreen.tsx
@@ -1,9 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import BottomNavBar from "../components/nav-bar/BottomNavBar";
 
 export default function ExploreScreen() {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Explore screen</Text>
+      <BottomNavBar />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between"
+  }
+});

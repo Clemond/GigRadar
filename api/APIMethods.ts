1114ccb52f17eeda6056bef419094d821e792503
Nicholas <quackshows11@gmail.com>
2025-05-09 19:10:56 +0200

    refactor: streamlined APIMethods

--- api/APIMethods.ts
+++ api/APIMethods.ts
@@ -1,50 +1,66 @@
+// api/APIMethods.ts
 import { APIConfig } from "./APIConfig";
 import { Get } from "./APIUtils";
 import { ITicketmasterSearchResponse } from "../types/ITicketmasterEvent";
-import { genreIdMap } from "../constants/genreIdMap";
 import { IGenreName } from "../types/IGenreName";
 import { getTodayDateRange } from "../utils/getTodaysDateRange";
+import { buildGenreParam, buildPageParam } from "../utils/buildQueryParams";
 
-export async function searchConcertsByCity(
+async function searchConcertsBase({
+  locationParam,
+  size,
+  page,
+  genreNames,
+  onlyToday
+}: {
+  locationParam: string;
+  size: number;
+  page?: number;
+  genreNames?: IGenreName[];
+  onlyToday?: boolean;
+}): Promise<ITicketmasterSearchResponse> {
+  const query = [
+    `${APIConfig.searchEvents}${APIConfig.key}`,
+    "&classificationName=music",
+    locationParam,
+    `&size=${size}`,
+    buildPageParam(page),
+    buildGenreParam(genreNames),
+    "&sort=date,asc",
+    getTodayDateRange(!!onlyToday)
+  ].join("");
+
+  return await Get<ITicketmasterSearchResponse>(query).then(({ data }) => data);
+}
+
+export function searchConcertsByCity(
   city: string,
   size: number,
   page?: number,
   genreNames?: IGenreName[],
   onlyToday: boolean = false
-): Promise<ITicketmasterSearchResponse> {
-  const pageParam = page !== undefined ? `&page=${page}` : "";
-
-  const genreIdParam =
-    genreNames && genreNames.length > 0
-      ? `&classificationId=${genreNames
-          .map((name) => genreIdMap[name])
-          .join(",")}`
-      : "";
-
-  const dateRangeParam = getTodayDateRange(onlyToday);
-
-  return await Get<ITicketmasterSearchResponse>(
-    `${APIConfig.searchEvents}${APIConfig.key}&classificationName=music&city=${city}&size=${size}${pageParam}${genreIdParam}&sort=date,asc${dateRangeParam}`
-  ).then(({ data }) => data);
+) {
+  return searchConcertsBase({
+    locationParam: `&city=${city}`,
+    size,
+    page,
+    genreNames,
+    onlyToday
+  });
 }
 
-export async function searchConcertsByCountry(
+export function searchConcertsByCountry(
   countryCode: string,
   page: number,
   size: number,
   genreNames?: IGenreName[],
   onlyToday: boolean = false
-): Promise<ITicketmasterSearchResponse> {
-  const genreIdParam =
-    genreNames && genreNames.length > 0
-      ? `&classificationId=${genreNames
-          .map((name) => genreIdMap[name])
-          .join(",")}`
-      : "";
-
-  const dateRangeParam = getTodayDateRange(onlyToday);
-
-  return await Get<ITicketmasterSearchResponse>(
-    `${APIConfig.searchEvents}${APIConfig.key}&classificationName=music&countryCode=${countryCode}&size=${size}&page=${page}${genreIdParam}&sort=date,asc${dateRangeParam}`
-  ).then(({ data }) => data);
+) {
+  return searchConcertsBase({
+    locationParam: `&countryCode=${countryCode}`,
+    size,
+    page,
+    genreNames,
+    onlyToday
+  });
 }

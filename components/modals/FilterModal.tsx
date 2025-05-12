0ef6a9068b5f12ccb6a58a27b2d90a40d8ae7d69
Nicholas <quackshows11@gmail.com>
2025-05-12 12:05:09 +0200

    feat: add list accordion for genre in FilterModal

--- components/modals/FilterModal.tsx
+++ components/modals/FilterModal.tsx
@@ -1,5 +1,7 @@
-import { Divider, Modal, Portal } from "react-native-paper";
+import { Divider, List, Modal, Portal, RadioButton } from "react-native-paper";
 import { Text, StyleSheet, View } from "react-native";
+import { AVAILABLE_GENRES } from "../../constants/genres";
+import { useState } from "react";
 
 interface filterModalProps {
   visible: boolean;
@@ -7,17 +9,42 @@ interface filterModalProps {
 }
 
 export default function FilterModal({ visible, hideModal }: filterModalProps) {
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+
+  const toggleGenre = (genre: string) => {
+    setSelectedGenres((prev) =>
+      prev.includes(genre) ? prev.filter((g) => g !== genre) : [...prev, genre]
+    );
+  };
+
   return (
     <Portal>
       <Modal
         visible={visible}
         onDismiss={hideModal}
         contentContainerStyle={styles.containerStyle}
+        style={{ justifyContent: "flex-start", marginTop: 20 }}
       >
         <View style={styles.filterHeader}>
           <Text style={styles.headerTitle}>Filter</Text>
           <Divider style={styles.divider} />
         </View>
+        <List.AccordionGroup>
+          <List.Accordion title="Genre" id="1">
+            {AVAILABLE_GENRES.map((genre, index) => (
+              <View key={index} style={styles.listItem}>
+                <List.Item title={genre} />
+                <RadioButton
+                  value={genre}
+                  status={
+                    selectedGenres.includes(genre) ? "checked" : "unchecked"
+                  }
+                  onPress={() => toggleGenre(genre)}
+                />
+              </View>
+            ))}
+          </List.Accordion>
+        </List.AccordionGroup>
       </Modal>
     </Portal>
   );
@@ -41,5 +68,11 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     width: "100%",
     backgroundColor: "#8CAFC5"
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginRight: 17
   }
 });
